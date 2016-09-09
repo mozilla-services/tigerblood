90987fc9278c4bb65bfaa2401ae4607f7efec248
Adrian Utrilla <adrianutrilla@gmail.com>
2016-09-09 13:13:42 -0700

    Added database insertion and selection, with benchmarks

--- db.go
+++ db.go
@@ -11,6 +11,11 @@ type DB struct {
 	*sql.DB
 }
 
+type ReputationEntry struct {
+	IP         string
+	Reputation uint
+}
+
 // NewDB creates a new DB instance from a DSN.
 func NewDB(dsn string) (*DB, error) {
 	db, err := sql.Open("postgres", dsn)
@@ -21,7 +26,7 @@ func NewDB(dsn string) (*DB, error) {
 	if err != nil {
 		return nil, err
 	}
-
+	db.SetMaxOpenConns(100)
 	return &DB{
 		DB: db,
 	}, nil
@@ -32,6 +37,11 @@ CREATE TABLE IF NOT EXISTS reputation (
 ip ip4r PRIMARY KEY NOT NULL,
 reputation int NOT NULL
 );
+CREATE INDEX IF NOT EXISTS reputation_ip_idx ON reputation USING gist (ip);
+`
+
+const emptyReputationTableSQL = `
+TRUNCATE TABLE reputation;
 `
 
 // CreateTables creates all the tables tigerblood needs, if they don't exist already
@@ -46,4 +56,25 @@ func (db DB) CreateTables() error {
 func (db DB) createReputationTable() error {
 	_, err := db.Query(createReputationTableSQL)
 	return err
+}
+
+func (db DB) emptyReputationTable() error {
+	_, err := db.Query(emptyReputationTableSQL)
+	return err
+}
+
+// InsertOrUpdateReputationEntry inserts a single ReputationEntry into the database
+func (db DB) InsertOrUpdateReputationEntry(tx *sql.Tx, entry ReputationEntry) error {
+	exec := db.Exec
+	if tx != nil {
+		exec = tx.Exec
+	}
+	_, err := exec("INSERT INTO reputation (ip, reputation) VALUES ($1, $2) ON CONFLICT DO NOTHING;", entry.IP, entry.Reputation)
+	return err
+}
+
+func (db DB) SelectSmallestMatchingSubnet(ip string) (ReputationEntry, error) {
+	var entry ReputationEntry
+	err := db.QueryRow("SELECT ip, reputation FROM reputation WHERE ip >>= $1 ORDER BY @ ip LIMIT 1;", ip).Scan(&entry.IP, &entry.Reputation)
+	return entry, err
 }
--- db_test.go
+++ db_test.go
@@ -1,8 +1,12 @@
 package tigerblood
 
 import (
+	"encoding/binary"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"log"
+	"math"
+	"math/rand"
 	"os"
 	"testing"
 )
@@ -24,4 +28,81 @@ func TestCreateSchema(t *testing.T) {
 	assert.Nil(t, err)
 	err = testDB.CreateTables()
 	assert.Nil(t, err, "Running CreateTables when the tables already exist shouldn't error")
+}
+
+func randomCidr(minSubnet, maxSubnet uint) string {
+	// Get a subnet with mean on 24 and a stdev of 5
+	subnet := math.Abs(rand.NormFloat64())*24 + 5
+	subnet = math.Min(subnet, float64(maxSubnet))
+	// The biggest subnets will be /8s.
+	subnet = math.Max(subnet, float64(minSubnet))
+	ip := rand.Uint32()
+	netmask := make([]byte, 4)
+	binary.BigEndian.PutUint32(netmask, ^((1 << (32 - uint(subnet))) - 1))
+	ipBytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(ipBytes, ip)
+	for i := range ipBytes {
+		ipBytes[i] &= netmask[i]
+	}
+	return fmt.Sprintf("%d.%d.%d.%d/%d", uint(ipBytes[0]), uint(ipBytes[1]), uint(ipBytes[2]), uint(ipBytes[3]), uint(subnet))
+}
+
+func BenchmarkInsertion(b *testing.B) {
+	err := testDB.emptyReputationTable()
+	assert.Nil(b, err)
+	b.RunParallel(func(pb *testing.PB) {
+		var ip [1000]string
+		generateRandomIps := func() {
+			b.StopTimer()
+			for i := range ip {
+				ip[i] = randomCidr(8, 32)
+			}
+			b.StartTimer()
+		}
+
+		generateRandomIps()
+		for i := 0; pb.Next(); i++ {
+			if i%1000 == 0 {
+				generateRandomIps()
+			}
+			currIP := ip[i%1000]
+			err := testDB.InsertOrUpdateReputationEntry(nil, ReputationEntry{
+				IP:         currIP,
+				Reputation: 50,
+			})
+			if err != nil {
+				b.Error(err)
+			}
+		}
+	})
+}
+
+func BenchmarkSelection(b *testing.B) {
+	err := testDB.emptyReputationTable()
+	assert.Nil(b, err)
+	tx, err := testDB.Begin()
+	assert.Nil(b, err)
+	var ip [1000]string
+	for i := 0; i < 10000; i++ {
+		if i%1000 == 0 {
+			for j := range ip {
+				ip[j] = randomCidr(8, 32)
+			}
+		}
+		currIP := ip[i%1000]
+		err := testDB.InsertOrUpdateReputationEntry(tx, ReputationEntry{
+			IP:         currIP,
+			Reputation: 50,
+		})
+		if err != nil {
+			b.Error(err)
+		}
+	}
+	tx.Commit()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			testDB.SelectSmallestMatchingSubnet(randomCidr(32, 32))
+		}
+	})
 }
