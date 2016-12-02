ec3ebd0d19d1a64febd5994ba46f04cf9a0d5c21
Greg Guthe <gguthe@mozilla.com>
2016-12-02 10:31:11 -0500

    publish go runtime stats to datadog

--- cmd/tigerblood/main.go
+++ cmd/tigerblood/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/DataDog/datadog-go/statsd"
+	_ "github.com/bmhatfield/go-runtime-metrics"
 	"github.com/spf13/viper"
+	"flag"
 	"go.mozilla.org/tigerblood"
 	"log"
 	"net/http"
@@ -15,12 +17,28 @@ func main() {
 	viper.SetDefault("BIND_ADDR", "127.0.0.1:8080")
 	viper.SetDefault("STATSD_ADDR", "127.0.0.1:8125")
 	viper.SetDefault("HAWK", false)
+	viper.SetDefault("PUBLISH_RUNTIME_STATS", false)
+	viper.SetDefault("RUNTIME_PAUSE_INTERVAL", 10)
+	viper.SetDefault("RUNTIME_CPU", true)
+	viper.SetDefault("RUNTIME_MEM", true)
+	viper.SetDefault("RUNTIME_GC", true)
+
 	viper.SetEnvPrefix("tigerblood")
 	viper.AutomaticEnv()
 	err := viper.ReadInConfig()
 	if err != nil {
 		log.Fatalf("Error loading config file: %s", err)
 	}
+
+	// Set flags for go-runtime-metrics
+	flag.Set("statsd", viper.GetString("STATSD_ADDR"))
+	flag.Set("metric-prefix", "tigerblood")
+	flag.Set("pause", viper.GetString("RUNTIME_PAUSE_INTERVAL"))
+	flag.Set("publish-runtime-stats", viper.GetString("PUBLISH_RUNTIME_STATS"))
+	flag.Set("cpu", viper.GetString("RUNTIME_CPU"))
+	flag.Set("mem", viper.GetString("RUNTIME_MEM"))
+	flag.Set("gc", viper.GetString("RUNTIME_GC"))
+
 	if !viper.IsSet("DSN") {
 		log.Fatalf("No DSN found. Cannot continue without a database")
 	}
@@ -34,6 +52,7 @@ func main() {
 	if viper.IsSet("STATSD_ADDR") {
 		statsdClient, err = statsd.New(viper.GetString("STATSD_ADDR"))
 		statsdClient.Namespace = "tigerblood."
+		flag.Parse() // kick off go-runtime-stats collector
 	} else {
 		log.Println("statsd not found")
 	}
