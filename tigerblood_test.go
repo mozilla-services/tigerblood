940ab2139b5c06418c4f00e3b1197c2d75e8d80a
Greg Guthe <gguthe@mozilla.com>
2017-03-06 16:00:23 -0500

    more route tests

--- tigerblood_test.go
+++ tigerblood_test.go
@@ -233,6 +233,16 @@ func TestListViolations(t *testing.T) {
 	assert.Equal(t, "{\"TestViolation\":90,\"TestViolation:2\":20}", string(body))
 }
 
+func TestListViolationsMissingViolationsMiddleware(t *testing.T) {
+	h := HandleWithMiddleware(NewRouter(), []Middleware{})
+	req := httptest.NewRequest("GET", "/violations", nil)
+	recorder := httptest.NewRecorder()
+	h.ServeHTTP(recorder, req)
+	res := recorder.Result()
+
+	assert.Equal(t, http.StatusInternalServerError, res.StatusCode)
+}
+
 func TestInsertReputationByViolation(t *testing.T) {
 	dsn, found := os.LookupEnv("TIGERBLOOD_DSN")
 	assert.True(t, found)
@@ -277,4 +287,49 @@ func TestInsertReputationByViolation(t *testing.T) {
 	recorder = httptest.ResponseRecorder{}
 	h.ServeHTTP(&recorder, httptest.NewRequest("PUT", "/violations////", strings.NewReader(`{"Violation": "UnknownViolation"}`)))
 	assert.Equal(t, http.StatusMovedPermanently, recorder.Code)
+}
+
+func TestLoadBalancerHeartbeatHandler(t *testing.T) {
+	h := HandleWithMiddleware(NewRouter(), []Middleware{})
+	req := httptest.NewRequest("GET", "/__lbheartbeat__", nil)
+	recorder := httptest.NewRecorder()
+	h.ServeHTTP(recorder, req)
+	res := recorder.Result()
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+}
+
+func TestHeartbeatHandler(t *testing.T) {
+	dsn, found := os.LookupEnv("TIGERBLOOD_DSN")
+	assert.True(t, found)
+	db, err := NewDB(dsn)
+	assert.Nil(t, err)
+
+	h := HandleWithMiddleware(NewRouter(), []Middleware{AddDB(db)})
+	req := httptest.NewRequest("GET", "/__heartbeat__", nil)
+	recorder := httptest.NewRecorder()
+	h.ServeHTTP(recorder, req)
+	res := recorder.Result()
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+}
+
+func TestHeartbeatHandlerWithoutDB(t *testing.T) {
+	h := HandleWithMiddleware(NewRouter(), []Middleware{})
+	req := httptest.NewRequest("GET", "/__heartbeat__", nil)
+	recorder := httptest.NewRecorder()
+	h.ServeHTTP(recorder, req)
+	res := recorder.Result()
+
+	assert.Equal(t, http.StatusInternalServerError, res.StatusCode)
+}
+
+func TestVersionHandler(t *testing.T) {
+	h := HandleWithMiddleware(NewRouter(), []Middleware{})
+	req := httptest.NewRequest("GET", "/__version__", nil)
+	recorder := httptest.NewRecorder()
+	h.ServeHTTP(recorder, req)
+	res := recorder.Result()
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
 }
