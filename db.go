f1dab6cfc71287501e58211c71842ef130bc0b02
Greg Guthe <gguthe@mozilla.com>
2017-03-29 17:03:31 -0400

    don't order by ip for reputation read
    For tiny table size triggers a sort:
    
    tigerblood=> EXPLAIN SELECT ip, reputation FROM reputation WHERE ip >>= '192.168.1.1' ORDER BY @ ip LIMIT 1;
                                             QUERY PLAN
    --------------------------------------------------------------------------------------------
     Limit  (cost=9.52..9.52 rows=1 width=12)
       ->  Sort  (cost=9.52..9.52 rows=2 width=12)
             Sort Key: ((@ ip))
             ->  Bitmap Heap Scan on reputation  (cost=4.16..9.51 rows=2 width=12)
                   Recheck Cond: (ip >>= '192.168.1.1'::ip4r)
                   ->  Bitmap Index Scan on reputation_ip_idx  (cost=0.00..4.16 rows=2 width=0)
                         Index Cond: (ip >>= '192.168.1.1'::ip4r)
    (7 rows)
    
    tigerblood=> EXPLAIN SELECT ip, reputation FROM reputation WHERE ip >>= '192.168.1.1' LIMIT 1;
                                             QUERY PLAN
    --------------------------------------------------------------------------------------------
     Limit  (cost=0.14..6.16 rows=1 width=12)
       ->  Index Scan using reputation_ip_idx on reputation  (cost=0.14..12.18 rows=2 width=12)
             Index Cond: (ip >>= '192.168.1.1'::ip4r)
    (3 rows)

--- db.go
+++ db.go
@@ -80,7 +80,7 @@ func NewDB(dsn string) (*DB, error) {
 	if err != nil {
 		return nil, fmt.Errorf("Could not create tables: %s", err)
 	}
-	reputationSelectStmt, err := db.Prepare("SELECT ip, reputation FROM reputation WHERE ip >>= $1 ORDER BY @ ip LIMIT 1;")
+	reputationSelectStmt, err := db.Prepare("SELECT ip, reputation FROM reputation WHERE ip >>= $1 LIMIT 1;")
 	if err != nil {
 		return nil, fmt.Errorf("Could not create prepared statement: %s", err)
 	}
