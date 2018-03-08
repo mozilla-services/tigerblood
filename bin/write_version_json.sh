0769e2d97a6bd6b0fe661102a47a966ef6d405a8
Greg Guthe <gguthe@mozilla.com>
2018-03-08 10:16:05 -0500

    bin: Add write_version_json.sh script

---
+++ bin/write_version_json.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+## writes a version.json file for the /__version__ endpoint
+
+# default variables
+: "${CIRCLE_SHA1=$(git rev-parse HEAD)}"
+: "${CIRCLE_TAG=$(git describe --tags)}"
+: "${CIRCLE_PROJECT_USERNAME=mozilla-services}"
+: "${CIRCLE_PROJECT_REPONAME=tigerblood}"
+: "${CIRCLE_BUILD_URL=localdev}"
+
+rm -f version.json
+
+printf '{"commit":"%s","version":"%s","source":"https://github.com/%s/%s","build":"%s"}\n' \
+            "$CIRCLE_SHA1" \
+            "$CIRCLE_TAG" \
+            "$CIRCLE_PROJECT_USERNAME" \
+            "$CIRCLE_PROJECT_REPONAME" \
+            "$CIRCLE_BUILD_URL" > version.json
