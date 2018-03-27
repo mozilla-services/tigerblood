9d712623aca05e0ec4586dec11f8c0fedc83f61e
Greg Guthe <gguthe@mozilla.com>
2018-03-27 15:16:03 -0400

    client: factor out status errors; normalize err to error

--- client.go
+++ client.go
@@ -13,6 +13,12 @@ import (
 	"go.mozilla.org/hawk"
 )
 
+var (
+	ClientUnexpectedGETStatusError = errors.New("Unexpected HTTP status from GET.")
+	ClientUnexpectedPUTStatusError = errors.New("Unexpected HTTP Status from PUT")
+	ClientUnexpectedPOSTStatusError = errors.New("Unexpected HTTP Status from POST")
+)
+
 // Client is an http.Client for the tigerblood service
 type Client struct {
 	*http.Client
@@ -50,33 +56,33 @@ func (client Client) SetReputation(cidr string, reputation uint) (*http.Response
 		IP:         cidr,
 		Reputation: reputation,
 	}
-	body, err := json.Marshal(entry)
-	if err != nil {
-		fmt.Printf("Error marshaling request JSON body: %s\n", err)
+	body, error := json.Marshal(entry)
+	if error != nil {
+		fmt.Printf("Error marshaling request JSON body: %s\n", error)
 		os.Exit(1)
 	}
 
-	req, err := http.NewRequest("POST", client.URL, bytes.NewReader(body))
+	req, error := http.NewRequest("POST", client.URL, bytes.NewReader(body))
 	client.AuthRequest(req, body)
-	resp, err := client.Do(req)
-	if err != nil {
-		return resp, err
+	resp, error := client.Do(req)
+	if error != nil {
+		return resp, error
 	}
 	if resp.StatusCode == http.StatusConflict {
 		fmt.Printf("Attempting update of IP since it already exists.\n")
 
-		req, err := http.NewRequest("PUT", client.URL+cidr, bytes.NewReader(body))
+		req, error := http.NewRequest("PUT", client.URL+cidr, bytes.NewReader(body))
 		client.AuthRequest(req, body)
-		resp, err := client.Do(req)
-		if err != nil {
-			return resp, err
+		resp, error := client.Do(req)
+		if error != nil {
+			return resp, error
 		}
 		if resp.StatusCode != http.StatusOK {
-			return resp, errors.New("Unexpected HTTP Status from PUT.")
+			return resp, ClientUnexpectedPUTStatusError
 		}
 		return resp, nil
 	} else if resp.StatusCode != http.StatusCreated {
-		return resp, errors.New("Unexpected HTTP Status from POST.")
+		return resp, ClientUnexpectedPOSTStatusError
 	}
 	return resp, nil
 }
@@ -84,7 +90,7 @@ func (client Client) SetReputation(cidr string, reputation uint) (*http.Response
 // BanIP sets the reputation for an IPv4 CIDR to 0 to block it for the maximum decay period
 func (client Client) BanIP(cidr string) (*http.Response, error) {
 	resp, error := client.SetReputation(cidr, 0)
-	if error == errors.New("Unexpected HTTP Status from POST.") {
+	if error == ClientUnexpectedPOSTStatusError {
 		fmt.Printf("Bad response banning IP:\n%+v\n", resp)
 		return resp, error
 	} else if error != nil {
@@ -97,7 +103,7 @@ func (client Client) BanIP(cidr string) (*http.Response, error) {
 // UnbanIP sets the reputation for an IPv4 CIDR to 100 to immediately unblock it
 func (client Client) UnbanIP(cidr string) (*http.Response, error) {
 	resp, error := client.SetReputation(cidr, 0)
-	if error == errors.New("Unexpected HTTP Status from POST.") {
+	if error == ClientUnexpectedPOSTStatusError {
 		fmt.Printf("Bad response unbanning IP:\n%+v\n", resp)
 	} else if error != nil {
 		return resp, error
@@ -107,32 +113,32 @@ func (client Client) UnbanIP(cidr string) (*http.Response, error) {
 
 // Exceptions requests the current exceptions list
 func (client Client) Exceptions() (*http.Response, error) {
-	req, err := http.NewRequest("GET",
+	req, error := http.NewRequest("GET",
 		strings.TrimRight(client.URL, "/")+"/exceptions", nil)
 	client.AuthRequest(req, []byte{})
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
+	resp, error := client.Do(req)
+	if error != nil {
+		return nil, error
 	}
 	if resp.StatusCode != http.StatusOK {
 		fmt.Printf("Bad response requesting exceptions:\n%+v\n", resp)
-		return resp, errors.New("Unexpected HTTP status from GET.")
+		return resp, ClientUnexpectedGETStatusError
 	}
 	return resp, nil
 }
 
 // Reputation requests the reputation score for an IP address
 func (client Client) Reputation(ipaddr string) (*http.Response, error) {
-	req, err := http.NewRequest("GET",
+	req, error := http.NewRequest("GET",
 		strings.TrimRight(client.URL, "/")+"/"+ipaddr, nil)
 	client.AuthRequest(req, []byte{})
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
+	resp, error := client.Do(req)
+	if error != nil {
+		return nil, error
 	}
 	if resp.StatusCode != http.StatusOK {
 		fmt.Printf("Bad response requesting reputation:\n%+v\n", resp)
-		return resp, errors.New("Unexpected HTTP status from GET.")
+		return resp, ClientUnexpectedGETStatusError
 	}
 	return resp, nil
 }
