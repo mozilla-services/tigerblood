4396643a4bffa1360d8cf6cc249a5995e35e042d
Aaron Meihm <ameihm@mozilla.com>
2018-05-11 15:49:35 -0500

    cleanup client code

--- client.go
+++ client.go
@@ -118,38 +118,27 @@ func (client Client) SetReviewed(cidr string, status bool) (*http.Response, erro
 // BanIP sets the reputation for an IPv4 CIDR to 0 to block it for the maximum decay period
 func (client Client) BanIP(cidr string) (*http.Response, error) {
 	// Since this is being applied from the ban command, set reviewed to true
-	resp, error := client.SetReputation(cidr, 0, true)
-	if error == ClientUnexpectedPOSTStatusError {
-		fmt.Printf("Bad response banning IP:\n%+v\n", resp)
-		return resp, error
-	} else if error != nil {
-		return resp, error
-	}
-	return resp, nil
+	return client.SetReputation(cidr, 0, true)
 }
 
 // UnbanIP sets the reputation for an IPv4 CIDR to 100 to immediately unblock it
 func (client Client) UnbanIP(cidr string) (*http.Response, error) {
-	resp, error := client.SetReputation(cidr, 100, false)
-	if error == ClientUnexpectedPOSTStatusError {
-		fmt.Printf("Bad response unbanning IP:\n%+v\n", resp)
-	} else if error != nil {
-		return resp, error
-	}
-	return resp, nil
+	return client.SetReputation(cidr, 100, false)
 }
 
 // Exceptions requests the current exceptions list
 func (client Client) Exceptions() (*http.Response, error) {
-	req, error := http.NewRequest("GET",
+	req, err := http.NewRequest("GET",
 		strings.TrimRight(client.URL, "/")+"/exceptions", nil)
+	if err != nil {
+		return nil, err
+	}
 	client.AuthRequest(req, []byte{})
-	resp, error := client.Do(req)
-	if error != nil {
-		return nil, error
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
 	}
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Bad response requesting exceptions:\n%+v\n", resp)
 		return resp, ClientUnexpectedGETStatusError
 	}
 	return resp, nil
@@ -157,15 +146,14 @@ func (client Client) Exceptions() (*http.Response, error) {
 
 // Reputation requests the reputation score for an IP address
 func (client Client) Reputation(ipaddr string) (*http.Response, error) {
-	req, error := http.NewRequest("GET",
+	req, err := http.NewRequest("GET",
 		strings.TrimRight(client.URL, "/")+"/"+ipaddr, nil)
 	client.AuthRequest(req, []byte{})
-	resp, error := client.Do(req)
-	if error != nil {
-		return nil, error
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
 	}
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Bad response requesting reputation:\n%+v\n", resp)
 		return resp, ClientUnexpectedGETStatusError
 	}
 	return resp, nil
